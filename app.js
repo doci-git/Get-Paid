6d42041b9167e1294ece55732ae848af98345496
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-11-27 08:26:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+
+function hide () {
+    // primo avisso in data 
+    const middleDate = new Date ('11/20/2022');
+    // secondo e ultimo aviso 
+    const futureDate = new Date('11/29/2022');
+    // data attuale 
+    const presentDate = new Date();
+}
